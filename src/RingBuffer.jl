59aa2a537ed712ad410b0ba0baf21a8a9b574735
Kevin Squire <kevin.squire@gmail.com>
2013-07-26 19:27:15 -0700

    Greatly simplified interface.

--- src/RingBuffer.jl
+++ src/RingBuffer.jl
@@ -1,49 +1,30 @@
 
-type IORBPos{T}
-    buffer::IORBBuffer{T}
-    ptr::Integer
-end
-
-type IORBReaderWriter{T} <: IO
-    ptr::IORBPos{T}
-    mark::IORBPos{T}
-    marklen::Integer
-    marked_elems::Integer
-    readable::Bool
-    writeable::Bool
-    seekable::Bool
-end
-
-type IORBBuffer{T}
-    ord::Integer
+type IOBuffer
+    bufnum::Int
+    data::Vector{Uint8}
     datasize::Integer
-    pinned::Bool
-    readercount::Integer
-    writercount::Integer
-    markcount::Integer
-    buffer::AbstractArray{T}
 end
 
-IORBBuffer{T}(size::Integer) = IORBBuffer{T}(0, 0, 0, 0, 0, False, Array(T, size))
-IORBBuffer(size::Integer) = IORBBuffer{Uint8}(size)
+IOBuffer(num::Int, bufsize::Int) = IOBuffer(num, Array(Uint8, bufsize), bufsize)
 
-type IORingBuffer{T}
-    buffers::Vector{IORBBuffer{T}}
-    readers::Vector{IORBReader}
-    writers::Vector{IORBWriter}
-    first::IORBBuffer{T}
-    last::IORBBuffer{T}
+type IORingBuffer
+    buffers::Vector{IOBuffer}
+    read_buffer::Int
+    read_ptr::Int
+    write_buffer::Int
+    write_ptr::Int
 end
 
-function IORingBuffer{T}(bufsize::Integer=65536, count::Integer=16)
-    buffers = Vector(IORBBuffer{T}, count)
+function IORingBuffer(bufsize::Int=65536, count::Int=16)
+    buffers = Vector(IOBuffer, count)
     for i in 1:count
-        buffer[i] = IORBBuffer{T}(bufsize)
+        buffer[i] = IOBuffer(i, bufsize)
     end
-    IORingBuffer{T}(buffers, IORBReader[], IORBWriter[], buffers[1], buffers[1])
+    IORingBuffer(buffers, 1, 1, 1, 1)
 end
 
 function open{T}(rb::IORingBuffer{T}, mode="r")
-    if mode == "r":
-        return IORBReaderWriter{T}(IORBPos(rb.first, 0), IORBPos(rb.first, 0), 0, 0, true, false, true)
+    readable = contains(mode, 'r') || contains(mode, 'a')
+    writeable = contains(mode, 'w') || contains(mode, 'a')
+    return IORBReaderWriter{T}(IORBPos(rb.first, 0), IORBPos(rb.first, 0), 0, 0, readable, writeable, true)
 end
